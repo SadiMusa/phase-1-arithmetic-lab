84f3da11a29e39e595d343dd3ac1b821f1e2b999
Sadi Musa <sadimusa00@gmail.com>
2024-03-19 17:38:33 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// index.js
 
-// Write your code here
+// Function to multiply two numbers
+function multiply(num1, num2) {
+  return num1 * num2; // Corrected implementation
+}
+
+// Function to generate a random integer greater than 0
+function random() {
+  return Math.floor(Math.random() * 100) + 1; // Generate random integer between 1 and 100
+}
+
+module.exports = { multiply, random };
+
+
